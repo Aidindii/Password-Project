a5d6ef2103f466cb0df70fae6de1ceb658e584db
Aidin <idinesmkhani10@gmail.com>
2022-10-07 13:34:09 +0330

    Adding header file
    a header file which contains definitions has been added to the project

---
+++ Code/Define.h
@@ -0,0 +1,23 @@
+// Keys Values
+#define Key_0_Val 0
+#define Key_1_Val 1
+#define Key_2_Val 2
+#define Key_3_Val 3
+#define Key_4_Val 4
+#define Key_5_Val 5
+#define Key_6_Val 6
+#define Key_7_Val 7
+#define Key_8_Val 8
+#define Key_9_Val 9
+#define Key_A_Val 12
+#define Key_B_Val 13
+#define Key_C_Val 14
+#define Key_D_Val 15
+#define Key_Star_Val 10            //Del char
+#define Key_Sharp_Val 11
+#define Key_Menu_Val Key_A_Val    //Menu
+#define Key_Enter_Val Key_B_Val   //Enter
+#define Key_Esc_Val Key_C_Val     //Esc
+#define Key_Zero_Val Key_D_Val    //Zero
+#define Key_Up_Val Key_2_Val      //Up
+#define Key_Down_Val Key_8_Val    //Down
